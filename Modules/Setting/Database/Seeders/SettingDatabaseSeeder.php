585a54c06fbd2bcbdfbfd14fddabf822632123d9
Ace Macariola <falconsys32@gmail.com>
2021-11-15 20:41:16 +0800

    QPC IMS System

---
+++ Modules/Setting/Database/Seeders/SettingDatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Setting\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Modules\Setting\Entities\Setting;
+
+class SettingDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Setting::create([
+            'company_name' => '316 QPC IMS',
+            'company_email' => 'company@test.com',
+            'company_phone' => '012345678901',
+            'notification_email' => 'notification@test.com',
+            'default_currency_id' => 1,
+            'default_currency_position' => 'prefix',
+            'footer_text' => 'Triangle Pos © 2021 || Developed by <strong>SparkSoft Solutions</strong>',
+            'company_address' => ''
+        ]);
+    }
+}
