003780045073598a0e7faf1327bec0086f0fd7af
Ace Macariola <falconsys32@gmail.com>
2021-11-22 16:40:45 +0800

    New Updates

--- Modules/Adjustment/Resources/views/create.blade.php
+++ Modules/Adjustment/Resources/views/create.blade.php
@@ -37,7 +37,7 @@
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <label for="reference">Delivery Number <span class="text-danger">*</span></label>
-                                    <input type="text" class="form-control" name="delivery_number" required>
+                                    <input type="text" class="form-control" name="delivery_number" requiredx>
                                 </div>
                             </div>
 
